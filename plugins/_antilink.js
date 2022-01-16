bda099646a6342e9a9d20593d6ee6837871ddca4
Uhdahlah <89252558+Uhdahlah@users.noreply.github.com>
2022-01-16 11:02:32 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,15 +1,15 @@
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+let linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/i
 module.exports = {
-  before(m, { isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe) return true
+  before(m, { isBotAdmin, isUser}) {
+    if (m.isBaileys && m.fromMe && m.isAdmin) return true
     let chat = global.db.data.chats[m.chat]
     let isGroupLink = linkRegex.exec(m.text)
 
     if (chat.antiLink && isGroupLink) {
-      m.reply('Hapus!!\n\nLink Grup terdeteksi')
+      m.reply('*ANTILINK*\n\nLink terdeteksi!!!')
       if (global.opts['restrict']) {
-        if (isAdmin || !isBotAdmin) return true
-        // this.groupRemove(m.chat, [m.sender])
+        if (!isBotAdmin || isUser) return true
+        ,this.groupRemove(m.chat, [m.sender])
       }
     }
     return true
