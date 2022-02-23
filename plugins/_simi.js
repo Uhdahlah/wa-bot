6b05d28f2837a61225a16e975313ce7d3b4d2a7b
Uhdahlah <89252558+Uhdahlah@users.noreply.github.com>
2022-02-23 23:44:50 +0700

    Change repo

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -7,7 +7,7 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned) {
         if (/^.*false|disable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(API('xteam', '/simsimi', { kata: encodeURIComponent(m.text) }, 'APIKEY'))
+        let res = await fetc(global.API('https://api.simsimi.net', '/v2/', { kata: encodeURIComponent(m.text) }, 'APIKEY'))
         if (!res.ok) return m.reply(`${res.status} ${res.statusText}`)
         let json = await res.json()
         if (!json.status) return m.reply(require('util').format(json))
