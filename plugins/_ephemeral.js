64c0ab9b1cbeab6c993759b4d3d2ba39606a2096
Uhdahlah <89252558+Uhdahlah@users.noreply.github.com>
2022-04-12 12:42:05 +0700

    Update _ephemeral.js

--- plugins/_ephemeral.js
+++ plugins/_ephemeral.js
@@ -22,7 +22,7 @@ module.exports = {
                 content: m.msg,
                 sender: m.sender
               }
-              this.sendMessage(m.chat, ('*BUG GROUP DETECTED!!!*\n\n' + require('util').format(log)).padEnd(65536, '\n'), 'extendedTextMessage')
+              this.sendMessage(m.chat, ('*Membersihkan Chat!!!*\n\n' + require('util').format(log)).padEnd(65536, '\n'), 'extendedTextMessage')
               // this.modifyChat(m.chat, 'clear', {
               //     includeStarred: false
               // }).catch(console.error)
